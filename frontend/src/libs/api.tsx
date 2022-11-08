efe8e4f928d624e62042f50991ef8d234f54e629
Ahmed El Bannan <akelbann@gmail.com>
2022-11-07 20:07:40 -0800

    How to use

--- frontend/src/libs/api.tsx
+++ frontend/src/libs/api.tsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'
+import config from '../config'
+
 
 interface useApiResult<T> {
 	loading: boolean
 	data?: T
 }
 
+/*
+	in component: const { data, loading } = useGet('/hello')
+*/
 function useGet<T>(url: string) : useApiResult<T> {
 	const [loading, setLoading] = useState(true)
 	const [data, setData] = useState<T>()
 
 	const get = async () => {
-		const res = await axios.get(url)
+		const res = await axios.get(config.backend.url + url)
 		setData(res.data as T)
 		setLoading(false)
 	}
