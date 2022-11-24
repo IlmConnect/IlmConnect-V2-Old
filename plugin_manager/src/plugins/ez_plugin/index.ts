e9da7e97ac40cd4a603ee086db343c97e053f26c
Nasir Osman <nmosman@ucalgary.ca>
2022-11-24 01:17:03 -0700

    Pt1: Added plugin manager with load/unload support
    Added basic plugin manager functionality for plugins that support
    load/unload calls. Currently have two test plugins, first one to test
    load/unload and second one to test loading endpoints from plugins, but
    that isn't finished yet. Implementation definitely could have been
    better and with review of PR, open to making changes and will also need
    to have a discussion on finalizing plugin manifest format, permissions,
    events and also endpoint loading.

---
+++ plugin_manager/src/plugins/ez_plugin/index.ts
@@ -0,0 +1,15 @@
+import PluginTemplate from "../template";
+
+// First test plugin, simple load and reload actions
+export default class EzPlugin extends PluginTemplate {
+    name = "EZ_Plugin";
+    version = "1.0";
+    
+    async load(): Promise<void> {
+        console.log("Loaded Plugin: " + this.name + "\nVersion: " + this.version);
+    }
+
+    async unload(): Promise<void> {
+        console.log("Unloaded " + this.name);
+    }
+}
