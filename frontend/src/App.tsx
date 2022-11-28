0f8c4aa142a23874de752e834c222b593bdffb0e
481Raafat <71669482+481Raafat@users.noreply.github.com>
2022-11-27 21:04:12 -0700

    cleaned front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import {Routes, Route} from "react-router-dom" 
-
-import CoursePage from './routes/coursePage';
-import CreateCourse from './routes/createCourse';
 
 function App() {
 	return (
 		<>
-		  <Routes>
-			  <Route path = "/course-page" element = {<CoursePage/>}/>
-			  <Route path = "/course-creation" element = {<CreateCourse/>}/>
-		  </Routes>
+
 		</>
 	  );
 }
