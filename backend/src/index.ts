6ef96e7bf51126f3230d69f6df9283eeb55baf9c
Ahmed El Bannan <akelbann@gmail.com>
2022-12-28 21:43:21 -0800

    zod

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,18 @@ import { PrismaClient } from '@prisma/client';
 import courseRoutes from './routes/courses';
 import authRoutes from './routes/auth';
 import cors from 'cors';
+import { createConfig } from 'express-zod-api'
+
+const config = createConfig({
+	server: {
+		listen: 8000,
+	},
+	cors: true,
+	logger: {
+		level: 'debug',
+		color: true,
+	}
+})
 
 const app: Express = express();
 app.use(express.json()); 
