178fb0c8b66982bda4f55cada8bfc5c7c6c8c19d
Ahmed El Bannan <akelbann@gmail.com>
2022-12-28 22:54:25 -0800

    Add zod server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,37 +3,46 @@ import { PrismaClient } from '@prisma/client';
 import courseRoutes from './routes/courses';
 import authRoutes from './routes/auth';
 import cors from 'cors';
-import { createConfig } from 'express-zod-api'
-
-const config = createConfig({
-	server: {
-		listen: 8000,
-	},
-	cors: true,
-	logger: {
-		level: 'debug',
-		color: true,
-	}
-})
+import { createConfig, createServer } from 'express-zod-api'
+import routes from './routes'
 
 const app: Express = express();
 app.use(express.json()); 
 app.use(cors()); 
 const prisma = new PrismaClient();
 
-const port = 8000;
+const config = createConfig({
+	server: {
+		listen: 8000,
+	},
+	cors: false,
+	logger: {
+		level: 'debug',
+		color: true,
+	},
+	startupLogo: false,
+})
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
-});
+createServer(
+	config,
+	routes(app, prisma)
+)
 
-// Initialize app courses
-courseRoutes(app, prisma);
-authRoutes(app, prisma);
 
-app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+
+// const port = 8000;
+
+// app.get('/', (req: Request, res: Response) => {
+// 	res.send('Express + TypeScript Server');
+// });
+
+// // Initialize app courses
+// courseRoutes(app, prisma);
+// authRoutes(app, prisma);
+
+// app.listen(port, () => {
+// 	console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+// });
 
 
 
