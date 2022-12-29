673645a37d4fa7a553824f488ff34553e406d35e
Ahmed El Bannan <akelbann@gmail.com>
2022-12-28 23:39:33 -0800

    Sign up validation!

--- frontend/src/views/auth/SignUpView/SignUpView.tsx
+++ frontend/src/views/auth/SignUpView/SignUpView.tsx
@@ -1,30 +1,35 @@
-import { Button, CircularProgress, Grid, styled, TextField, Typography, Unstable_Grid2 } from '@mui/material';
-import axios from 'axios';
-import { observer } from 'mobx-react-lite';
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import config from 'config';
-import authStore from 'store/auth';
-import FullScreenGrid from '../components/FullScreenGrid/FullScreenGrid';
+import { Button, CircularProgress, TextField, Typography } from '@mui/material';
+import axios from 'axios'
+import { observer } from 'mobx-react-lite'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import config from 'config'
+import authStore from 'store/auth'
+import FullScreenGrid from '../components/FullScreenGrid/FullScreenGrid'
+import { useForm } from "react-hook-form"
 
-
+interface FormData {
+	email: string
+	password: string
+}
 
 function SignUpView() {
-	const navigate = useNavigate();
-	const [loading, setLoading] = useState(false);
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [error, setError] = useState(false);
-
-	const signUp = async () => {
+	const { register, formState: { errors }, handleSubmit } = useForm<FormData>()
+	const navigate = useNavigate()
+	const [loading, setLoading] = useState(false)
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	console.log(errors)
+	const signUp = async (data: FormData) => {
+		
 		try {
 			setLoading(true);
-			const res = await axios.post(config.backend.url + 'signup', { email, password });
-			authStore.setUser(res.data?.user, res.data?.token);
+			const res = await axios.post(config.backend.url + 'signup', data)
+			authStore.setUser(res.data?.user, res.data?.token)
 			navigate('/');
 		}
 		catch (e) {
-			setError(true);
+			alert('There was an error singing you up. Please try again later' + e)
 		}
 		finally {
 			setLoading(false);
@@ -45,22 +50,52 @@ function SignUpView() {
 			<TextField 
 				id="email"
 				label="Email"
-				value={email}
-				onChange={e => setEmail(e.target.value)}
+				{...register(
+					'email',
+					{ 
+						required: { 
+							value: true, 
+							message: 'Email is required' 
+						},
+						pattern: {
+							value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+							message: 'Must be a valid email'
+						}
+					}
+				)}
+				error={!!errors.email}
+				helperText={errors.email?.message}
 				disabled={loading}
 			/>
 			<TextField 
 				id="password"
 				label="Password"
 				type="password"
-				value={password}
-				onChange={e => setPassword(e.target.value)}
+				{...register(
+					'password',
+					{
+						required: {
+							value: true, 
+							message: 'Password is required' 
+						},
+						minLength: {
+							value: 8, 
+							message: 'Password must be longer than 8 characters' 
+						},
+						pattern: {
+							value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/,
+							message: 'Password must contain one uppercase letter, one lowercase letter, and one special character(#?!@$%^&*-)'
+						}
+					}
+				)}
+				error={!!errors.password}
+				helperText={errors.password?.message}
 				disabled={loading}
 			/>
 
 			<Button
 				variant="contained"
-				onClick={signUp}
+				onClick={handleSubmit(signUp)}
 				disabled={loading}
 			>
 				{ loading? 
@@ -71,7 +106,7 @@ function SignUpView() {
 					'Sign Up' 
 				}
 			</Button>
-
+			
 			<Typography>
 				Already have an account?&nbsp;
 				<Link to="/login">
