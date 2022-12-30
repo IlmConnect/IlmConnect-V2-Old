cf467798c4e19758f8922e9f4a5badcd8131e8fe
481Raafat <71669482+481Raafat@users.noreply.github.com>
2022-12-30 13:49:40 -0700

    frontend routing

--- frontend/src/RouteRoot.tsx
+++ frontend/src/RouteRoot.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Page from './components/page/Page';
 import DrawerLayout from './layouts/Drawer';
-import CreateCourse from './routes/createCourse';
 
 import DynamicForm from 'components/DynamicForm/DynamicForm';
 import SignUpView from './views/auth/SignUpView/SignUpView';
@@ -30,8 +29,9 @@ export default function RouteRoot() {
 				<Route path='/tab1' element = {<Page pageName='tab1'></Page>}/>
 				<Route path='/tab2' element = {<Page pageName='tab2'></Page>}/>
 				<Route path='/tab3' element = {<Page pageName='tab3'></Page>}/>
-				<Route path='/course-creation' element = {<CreateCourse/>}/>
-				{/* <Route path='*'element = {<Navigate to='/tab1' replace></Navigate>}/> */}
+
+				<Route path='/form/create' element = {<DynamicForm/>}/>
+
                 <Route path='*' element = {<Dashboard/>}/>
 			</Routes>
 		</div>
