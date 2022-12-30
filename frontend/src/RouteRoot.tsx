a286e90bf4328afdcbf7099f4688b9765b6457f8
Ahmad Tahir <ahmad.tahir1995@gmail.com>
2022-12-29 18:20:32 -0700

    added a minidrawer.

--- frontend/src/RouteRoot.tsx
+++ frontend/src/RouteRoot.tsx
@@ -4,6 +4,7 @@ import DrawerLayout from './layouts/Drawer';
 import CreateCourse from './routes/createCourse';
 import SignUpView from './views/auth/SignUpView/SignUpView';
 import LogInView from './views/auth/LoginView/LoginView';
+import Dashboard from 'views/Dashboard/Dashboard';
 
 export default function RouteRoot() {
 	return (
@@ -28,7 +29,8 @@ export default function RouteRoot() {
 				<Route path='/tab2' element = {<Page pageName='tab2'></Page>}/>
 				<Route path='/tab3' element = {<Page pageName='tab3'></Page>}/>
 				<Route path='/course-creation' element = {<CreateCourse/>}/>
-				<Route path='*'element = {<Navigate to='/tab1' replace></Navigate>}/>
+				{/* <Route path='*'element = {<Navigate to='/tab1' replace></Navigate>}/> */}
+                <Route path='*' element = {<Dashboard/>}/>
 			</Routes>
 		</div>
 	);
