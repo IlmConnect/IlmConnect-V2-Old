efbe79356d8a2d18cfc73ce40ed60c67e518528d
Ahmed El Bannan <akelbann@gmail.com>
2023-01-03 18:37:30 -0800

    wip

--- frontend/src/components/MiniDrawer/MiniDrawer.tsx
+++ frontend/src/components/MiniDrawer/MiniDrawer.tsx
@@ -147,101 +147,101 @@ function MiniDrawer(props) {
         </DrawerHeader>
         <Divider />
         <List>
-          {[{ text: 'Dashboard', icon: <DashboardIcon />, child: [{text: 'DashboardChild', icon: <DashboardIcon />, child: []}] },
-            { text: 'Announcement', icon: <AnnouncementIcon />, child: [] },
-            { text: 'Users', icon: <GroupIcon />, child: [] },
-            { text: 'Programs', icon: <LibraryBooksIcon />, child: [] },
-            { text: 'My Bookings', icon: <TodayIcon />, child: [] },
-            { text: 'Settings', icon: <SettingsIcon />, child: [] }
+          {[{ text: 'Dashboard', icon: <DashboardIcon />, child: [{ text: 'DashboardChild', icon: <DashboardIcon />, child: [] }] },
+          { text: 'Announcement', icon: <AnnouncementIcon />, child: [] },
+          { text: 'Users', icon: <GroupIcon />, child: [] },
+          { text: 'Programs', icon: <LibraryBooksIcon />, child: [] },
+          { text: 'My Bookings', icon: <TodayIcon />, child: [] },
+          { text: 'Settings', icon: <SettingsIcon />, child: [] }
           ].map((sidebarItem) => (
-            <MiniDrawerItem sidebarItem={sidebarItem} isOpen={props.isOpen}/>
+            <MiniDrawerItem sidebarItem={sidebarItem} isOpen={props.isOpen} />
           ))}
         </List>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
         <div role="presentation" onClick={handleClick}>
-      <Breadcrumbs aria-label="breadcrumb">
-        <Link underline="hover" color="inherit" href="/">
-          Make
-        </Link>
-        <Link
-          underline="hover"
-          color="inherit"
-          href="/material-ui/getting-started/installation/"
-        >
-          This work
-        </Link>
-        <Typography color="text.primary">Breadcrumbs</Typography>
-      </Breadcrumbs>
-    </div>
-    <Box sx={{ width: '100%' }}>
-      <Stack
-        direction="row"
-        justifyContent="center"
-        alignItems="stretch"
-        spacing={2}
-      >
-        <Item sx={{ minWidth: 500}}>Item 1</Item>
-        <Item sx={{ maxWidth: 500}}>Item 2</Item>
-      </Stack>
-    </Box>
-      <Grid container spacing={2}>
-        <Grid xs={8} container> 
-          <Item sx={{ minWidth: 275, maxWidth: 500}}>    
-            <Card>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-          </Item>
-          <Item sx={{ minWidth: 275, maxWidth: 500}}>        
-          <Card>
-            <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="div">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                adjective
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-          <br/><br/> 
-        </Item>
-          <Item>Main Content <br/><br/> </Item>
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              Make
+            </Link>
+            <Link
+              underline="hover"
+              color="inherit"
+              href="/material-ui/getting-started/installation/"
+            >
+              This work
+            </Link>
+            <Typography color="text.primary">Breadcrumbs</Typography>
+          </Breadcrumbs>
+        </div>
+        <Box sx={{ width: '100%' }}>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="stretch"
+            spacing={2}
+          >
+            <Item sx={{ minWidth: 500 }}>Item 1</Item>
+            <Item sx={{ maxWidth: 500 }}>Item 2</Item>
+          </Stack>
+        </Box>
+        <Grid container spacing={2}>
+          <Grid xs={8} container>
+            <Item sx={{ minWidth: 275, maxWidth: 500 }}>
+              <Card>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    be{bull}nev{bull}o{bull}lent
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Item>
+            <Item sx={{ minWidth: 275, maxWidth: 500 }}>
+              <Card>
+                <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    be{bull}nev{bull}o{bull}lent
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+              <br /><br />
+            </Item>
+            <Item>Main Content <br /><br /> </Item>
+          </Grid>
+          <Grid xs={4}>
+            Side Content <br /> This section will summarize upcoming dates either in a list or calendar view. Maybe the timeline MUI component
+
+          </Grid>
         </Grid>
-        <Grid xs={4}>
-          Side Content <br/> This section will summarize upcoming dates either in a list or calendar view. Maybe the timeline MUI component
-          
-        </Grid>
-      </Grid>
         <Typography paragraph>
           Coming Soon
         </Typography>
